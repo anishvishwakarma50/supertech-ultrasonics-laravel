9236e41b0fc8782704b705fcfbc1052dbcd68d43
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-02-24 16:07:52 +0530

    Add Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Industry;
+use App\Models\Inquiry;
+use App\Models\Lead;
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\SiteContent;
+use App\Models\Slider;
+use App\Models\Specification;
+use App\Models\Testimonial;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,10 +26,34 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        // this will create the user for testing
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => 'admin@123'
         ]);
+        
+        // Slider Seeding
+        Slider::factory(3)->create();
+
+        // Industry Seeding
+        Industry::factory(5)->create();
+
+        // Site content factory
+        SiteContent::factory()->create();
+
+        // Testimonial Factory
+        Testimonial::factory(5)->create();
+
+        // Product, Product_Image, Specification, Inquiry and Lead Factory
+        Inquiry::factory(3)
+        ->for(Lead::factory())
+        ->for(Product::factory()
+                ->has(ProductImage::factory()
+                ->count(5)
+                ->Sequence(fn(Sequence $sequence) => ['position' => $sequence->index % 5 + 1])
+                , 'images')
+                ->for(Specification::factory())
+        )->create();
     }
 }
