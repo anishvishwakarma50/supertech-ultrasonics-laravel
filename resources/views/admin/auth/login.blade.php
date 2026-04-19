971ef6aa2c321407acd1f5e94a6b23b00e66d3c2
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-04-19 22:37:44 +0530

    File Update

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -7,7 +7,7 @@
                     <div class="col-lg-4 mx-auto">
                     <div class="auth-form-light text-left py-5 px-4 px-sm-5">
                         <div class="brand-logo">
-                        <img src="assets2/images/logo.svg" alt="logo">
+                        <img src="{{ Storage::url($siteData->logo) }}" alt="logo">
                         </div>
                         <h4>Hello! let's get started</h4>
                         <h6 class="font-weight-light">Sign in to continue.</h6>
@@ -21,19 +21,6 @@
                         </div>
                         <div class="mt-3 d-grid gap-2">
                             <button type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">Login</button>
-                        </div>
-                        <div class="my-2 d-flex justify-content-between align-items-center">
-                            <div class="form-check">
-                            <label class="form-check-label text-muted">
-                                <input type="checkbox" class="form-check-input"> Keep me signed in </label>
-                            </div>
-                            <a href="#" class="auth-link text-black">Forgot password?</a>
-                        </div>
-                        <div class="mb-2 d-grid gap-2">
-                            <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                            <i class="ti-facebook me-2"></i>Connect using facebook </button>
-                        </div>
-                        <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="register.html" class="text-primary">Create</a>
                         </div>
                         </form>
                     </div>
