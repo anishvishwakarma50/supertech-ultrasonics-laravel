a6087f86eaea7065c876cc2249aae944f36e8f5c
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-04-30 23:13:26 +0530

    File Update

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -6,7 +6,7 @@
 <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>{{ $title }}</title> 
+        <title>{{ $title }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- SEO Meta Tags --}}
